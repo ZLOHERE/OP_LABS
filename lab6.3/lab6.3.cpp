333da50ac512f342049e07ad135ee8b18ea4fd19
ZLOHERE <sasha.tushentsov123@gmail.com>
2021-12-23 19:25:28 +0300

    6.2.2 5-mark work

---
+++ lab6.3/lab6.3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<int> q2;
+    queue<int> q3;
+    queue<int> q5;
+    queue<int> res;
+    q2.push(2);
+    q3.push(3);
+    q5.push(5);
+    int cnt;
+    cin >> cnt;
+    for (int i = 0; i < cnt; i++)
+    {
+        int minimum;
+        minimum = min(q5.front(), min(q2.front(), q3.front()));
+        if ((minimum==q2.front()) or (minimum == q3.front()) or (minimum == q5.front()))
+        {
+            q2.push(minimum * 2);
+            q3.push(minimum * 3);
+            q5.push(minimum * 5);
+        }
+        if (minimum==q2.front())
+        {
+            q2.pop();
+        }
+        if (minimum == q3.front())
+        {
+            q3.pop();
+        }
+        if (minimum == q5.front())
+        {
+            q5.pop();
+        }
+        res.push(minimum);
+    }
+    int sizeqe = res.size();
+    for (int i = 0; i < sizeqe; i++)
+    {
+        cout << res.front()<<" ";
+        res.pop();
+    }
+}
---
+++ lab6.4/lab6.4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <queue>
+#include<string>
+using namespace std;
+int main()
+{
+    unordered_set<string> login;
+    queue<int> password;
+    int last_size, pass, cnt;
+    string log;
+    last_size = login.size();
+    cin >> cnt;
+    for (int i = 0; i < cnt; i++)
+    {
+        cin >> log;
+        login.insert(log);
+        cin >> pass;
+        if (last_size!=(login.size()))
+        {
+            password.push(pass);
+            last_size = login.size();
+        }
+    }
+    for (auto it = login.begin(); it != login.end(); it++)
+    {
+        cout << *it << "\t";
+    }
+    cout << endl;
+    for (int i = 0; i < last_size; i++)
+    {
+        cout << password.front() <<"\t";
+        password.pop();
+    }
+
+}
